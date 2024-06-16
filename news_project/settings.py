0656eb7726c60869ae53e1387550fa7f3f07b0c9
Jasur Kenjayev <94048770+Jasur-Kenjayev@users.noreply.github.com>
2024-06-16 11:42:03 +0500

    Update settings.py

--- news_project/settings.py
+++ news_project/settings.py
@@ -114,8 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/newsterm/news-feed.uz/django/staticfiles'
-STATICFILES_DIRS = ('/home/newsterm/news-feed.uz/django/static',)
+STATIC_ROOT = '/home/newsfedu/news-feed.uz/django/staticfiles'
+STATICFILES_DIRS = ('/home/newsfedu/news-feed.uz/django/static',)
 
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 # STATICFILES_DIRS = [BASE_DIR / 'static']
@@ -127,7 +127,7 @@ STATICFILES_FINDERS = [
 
 MEDIA_URL = 'media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_ROOT = '/home/newsterm/news-feed.uz/django/media'
+MEDIA_ROOT = '/home/newsfedu/news-feed.uz/django/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
